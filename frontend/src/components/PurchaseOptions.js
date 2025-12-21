3a54f8db3448c0dc7620ef60c57f961605c04c2a
PRAGADEESH G <150640941+PRAGADEESH2903@users.noreply.github.com>
2025-12-21 12:50:54 +0530

    PurchaseOptions.js

--- frontend/src/components/PurchaseOptions.js
+++ frontend/src/components/PurchaseOptions.js
@@ -15,8 +15,9 @@ import {
   Alert,
 } from "@mui/material";
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5050";
+export const API_BASE_URL =
+  process.env.REACT_APP_API_URL || "https://supabaseupdated.onrender.com";
+
 
 /* ================= VALIDATION ================= */
 const validationSchema = Yup.object({
