f2fcfedc0f3e1c6faa7a897882a7bf094a254b2c
PRAGADEESH G <150640941+PRAGADEESH2903@users.noreply.github.com>
2025-12-21 13:04:20 +0530

    ServiceDetails.js

--- frontend/src/components/ServiceDetails.js
+++ frontend/src/components/ServiceDetails.js
@@ -18,8 +18,8 @@ import {
 } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
-export const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "https://supabaseupdated.onrender.com";
+import { API_BASE_URL } from "../config";
+
 
 
 const validationSchema = Yup.object({
