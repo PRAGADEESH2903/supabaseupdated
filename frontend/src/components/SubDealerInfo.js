50fc497c13c4899fba8b98111c1ed67be5d9c573
PRAGADEESH G <150640941+PRAGADEESH2903@users.noreply.github.com>
2025-12-21 12:51:55 +0530

    SubDealerInfo.js

--- frontend/src/components/SubDealerInfo.js
+++ frontend/src/components/SubDealerInfo.js
@@ -10,8 +10,9 @@ import {
   Alert,
 } from "@mui/material";
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5050";
+export const API_BASE_URL =
+  process.env.REACT_APP_API_URL || "https://supabaseupdated.onrender.com";
+
 
 const SubDealerInfo = () => {
   const [dealers, setDealers] = useState([]);
